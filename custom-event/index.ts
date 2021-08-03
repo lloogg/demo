c089281f86382bfe9a0c7e3a6e0eed206ae0fd5f
septlog <septlog@qq.com>
2021-08-03 15:38:15 +0800

    namespace.

--- custom-event/index.ts
+++ custom-event/index.ts
@@ -5,6 +5,9 @@ let obj = {
   map: new Map<Element, Map<string, Function[]>>(),
   unique: '.x1',
   query(el: Element) {
+    if (!el) {
+      return this;
+    }
     if (!this.map.has(el)) {
       this.map.set(el, new Map<string, Function[]>());
     }
@@ -12,6 +15,9 @@ let obj = {
     return this;
   },
   on(name: string, fn: Function, data?: any) {
+    if (!this.el) {
+      return this;
+    }
     // 改成了 custom event，添加对额外数据支持
     let eventName = getEventName(name);
     // 如果名称和 dom event 的名称一致就会起冲突，所以这里加了一个 unique 字符串
@@ -24,6 +30,7 @@ let obj = {
     };
     // jquery 里面同一个方法注册多次，将触发多次，但是 addEventListener 同一个方法注册多次，只会触发一次
     // 这里我把方法写入了 fnn 中，每次调用 on 方法，注册的函数是独立的
+    fnn = fnn.bind(this);
     this.el.addEventListener(eventName, fnn);
     let listener = this.map.get(this.el);
     if (!listener.get(actualName)) {
@@ -32,19 +39,60 @@ let obj = {
     listener.get(actualName).push({ fn, fnn });
     return this;
   },
+
   off(name: string, fn: Function) {
     let listener = this.map.get(this.el);
+    let names = name.split(' ');
+    console.log(names);
+    names.forEach((name) => {
+      if (name.startsWith('.')) {
+        if (fn) {
+          listener.forEach((value, key) => {
+            if (key.endsWith(name)) {
+              let eventName = getEventName(key);
+              listener.get(key).forEach((item) => {
+                if (item.fn === fn) {
+                  this.el.removeEventListener(eventName, item.fnn);
+                }
+              });
+              let temp = listener.get(key).filter((item) => {
+                return item.fn !== fn;
+              });
+              if (temp.length === 0) {
+                listener.delete(key);
+              } else {
+                listener.set(key, temp);
+              }
+            }
+          });
+        } else {
+          listener.forEach((value, key) => {
+            if (key.indexOf(name) !== -1) {
+              let eventName = getEventName(key);
+              listener.get(key).forEach((item) => {
+                this.el.removeEventListener(eventName, item.fnn);
+              });
+              listener.delete(key);
+            }
+          });
+        }
+      } else {
+        this.unmount(name, fn);
+      }
+    });
+
+    return this;
+  },
+  unmount(name: string, fn: Function) {
+    let listener = this.map.get(this.el);
     let eventName = getEventName(name);
-    let actualName = eventName === name ? name + this.unique : name;
     if (fn) {
       // 如果名字相同，把所有该名字 match 的方法都移除
       if (eventName === name) {
         listener.forEach((item, key) => {
           if (key.startsWith(name)) {
-            console.log(key);
             listener.get(key).forEach((item) => {
               if (item.fn === fn) {
-                console.log(true);
                 this.el.removeEventListener(eventName, item.fnn);
               }
             });
@@ -55,6 +103,9 @@ let obj = {
           }
         });
       } else {
+        if (!listener.get(name)) {
+          return this;
+        }
         listener.get(name).forEach((item) => {
           if (item.fn === fn) {
             this.el.removeEventListener(eventName, item.fnn);
@@ -73,23 +124,25 @@ let obj = {
             listener.get(key).forEach((k) => {
               this.el.removeEventListener(eventName, k.fnn);
             });
-            listener.set(key, []);
+            listener.delete(key);
           }
         });
       } else {
-        console.log(name);
+        if (!listener.get(name)) {
+          return this;
+        }
         listener.get(name).forEach((item) => {
           this.el.removeEventListener(eventName, item.fnn);
         });
-        listener.set(name, []);
+        listener.delete(name);
       }
     }
-    return this;
   },
 };
 let query = obj.query.bind(obj);
-function fn(e) {
-  console.log(e.detail);
+export { query };
+function fn() {
+  console.log('ok');
 }
 function fnnn(e) {
   console.log(e.detail);
@@ -100,7 +153,12 @@ let root = document.querySelector('.root');
 // test2();
 // test3();
 // test4();
-test5();
+// test5();
+// testNamespace3();
+// testNamespace4();
+// testNamespace5();
+// testNamespace6();
+testNamespace7();
 function test1() {
   query(root).on('click.v0', fn, { data: 'data' });
   query(root).on('click.v0', fn, { data: 'data' });
@@ -144,4 +202,80 @@ function test4() {
 
 function test5() {
   query(root).off('click');
+}
+
+function testNamespace1() {
+  query(root).on('click.v1', fn);
+  query(root).on('click.v1', fn);
+  query(root).on('click.v1', fn);
+  query(root).on('click.v1', fn);
+  query(root).off('.v1', fn);
+}
+
+function testNamespace2() {
+  query(root).on('click.v1', fn);
+  query(root).on('click.v1', fn);
+  query(root).on('click.v1', fn);
+  query(root).on('mouseenter.v1', fn);
+  query(root).off('.v1', fn);
+}
+
+function testNamespace3() {
+  query(root).on('click.v1', fn);
+  query(root).on('click.v1', fn);
+  query(root).on('click.v1', fn);
+  query(root).on('mouseenter.v1', () => {
+    console.log('ok');
+  });
+  query(root).off('.v1', fn);
+  // 保留 mouseenter
+  console.log(query(root));
+}
+
+function testNamespace4() {
+  query(root).on('click.v1', fn);
+  query(root).on('click.v1', fn);
+  query(root).on('click.v1', fn);
+  query(root).on('mouseenter.v1', () => {
+    console.log('ok');
+  });
+  query(root).off('.v1');
+  console.log(query(root));
+  // 全部清除
+}
+
+function testNamespace5() {
+  // * pass
+  query(root).on('click.v1', fn);
+  query(root).on('click.v2', fn);
+  query(root).on('click.v1.v2', fn);
+  query(root).on('mouseenter.v1', () => {
+    console.log('ok');
+  });
+  query(root).off('click .v1.v2');
+  console.log(query(root));
+  // click 事件全部清除，mouseenter保留
+}
+
+function testNamespace6() {
+  // * pass
+  query(root).on('click.v1', fn);
+  query(root).on('click.v2', fn);
+  query(root).on('click.v1.v2', fn);
+  query(root).on('mouseenter.v1', () => {
+    console.log('ok');
+  });
+  query(root).off('.v1');
+  console.log(query(root));
+  // click 事件清除两个，mouseenter全部清除
+}
+
+function testNamespace7() {
+  // * fail
+  query(root).on('click.v1', fn);
+  query(root).on('click.v1', fn);
+  query(root).on('click.v1.v2', fn);
+  query(root).on('click.v1.v2', fn);
+  query(root).off('click.v2', fn);
+  console.log(query(root));
 }
