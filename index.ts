8231e43cb522a7aca5e66d27cdb5eeb8b5aca76e
septlog <septlog@qq.com>
2021-12-27 21:12:44 +0800

    pointer-events bounding-box

--- index.ts
+++ index.ts
@@ -1,18 +1,11 @@
 let xmlns = 'http://www.w3.org/2000/svg';
 
-document.addEventListener('mouseup', () => {
-  document.onmousemove = null;
-});
 interface Point {
   x: number;
   y: number;
 }
-type DirType = 'horizontal' | 'vertical';
-type DirInfo = 'top' | 'right' | 'bottom' | 'left';
-interface Dir {
-  type: DirType;
-  info: DirInfo;
-}
+type Dir = 'top' | 'right' | 'bottom' | 'left';
+
 interface Line {
   dir: Dir;
   source: Point;
@@ -26,7 +19,12 @@ class Edge {
   path: SVGPathElement = null;
   points: Point[] = [];
   lines: Line[] = [];
+  arrowStart: SVGPathElement = null;
+  // arrowStart 不闭合，因此点击不能触发事件
+  arrowStartHandle: SVGPathElement = null;
   arrowEnd: SVGPathElement = null;
+  firstLine: Line = null;
+  lastLine: Line = null;
   constructor(d: string) {
     this.d = d;
     this.g = document.createElementNS(xmlns, 'g') as SVGGElement;
@@ -50,7 +48,9 @@ class Edge {
     this.svg.appendChild(this.g);
 
     document.addEventListener('mouseup', () => {
-      document.removeEventListener('mousemove', this.dragPath.apply(this));
+      // document.removeEventListener('mousemove', this.dragPath.apply(this));
+      document.onmousemove = null;
+      document.body.style.cursor = 'initial';
     });
 
     // 提取纯数字
@@ -79,7 +79,16 @@ class Edge {
       });
     }
 
+    // 设置第一条线
+    this.firstLine = this.lines[0];
+    // 设置最后一条线
+    this.lastLine = this.lines[this.lines.length - 1];
+
+    this.addArrowStart();
+    this.drawArrowStart();
+    this.addArrowEnd();
     this.drawArrowEnd();
+
     this.handle.addEventListener('mousedown', (e) => {
       console.log('handle mousedown');
       let x = e.offsetX;
@@ -95,45 +104,51 @@ class Edge {
         let xMax = source.x < target.x ? target.x : source.x;
         let yMin = source.y < target.y ? source.y : target.y;
         let yMax = source.y < target.y ? target.y : source.y;
-        if (line.dir.type === 'horizontal') {
+        if (line.dir === 'left' || line.dir === 'right') {
           // 线是水平的，那么 source.y === target.y
           if (x < xMax && x > xMin && Math.abs(y - source.y) < 5) {
             chosenLine = line;
             break;
           }
-        } else if (line.dir.type === 'vertical') {
+        } else if (line.dir === 'top' || line.dir === 'bottom') {
           if (y < yMax && y > yMin && Math.abs(x - source.x) < 5) {
             chosenLine = line;
             break;
           }
         }
       }
-      // document.onmousemove = (e) => {
-      //   let x = e.offsetX;
-      //   let y = e.offsetY;
-      //   if (chosenLine) {
-      //     if (chosenLine.dir.type === 'horizontal') {
-      //       chosenLine.source.y = y;
-      //       chosenLine.target.y = y;
-      //     } else if (chosenLine.dir.type === 'vertical') {
-      //       chosenLine.source.x = x;
-      //       chosenLine.target.x = x;
-      //     }
-      //   }
-      //   let newD = '';
-      //   for (let i = 0; i < this.points.length; i++) {
-      //     let point = this.points[i];
-      //     if (i === 0) {
-      //       newD += `M ${point.x} ${point.y}`;
-      //     } else {
-      //       newD += ` L ${point.x} ${point.y}`;
-      //     }
-      //   }
-      //   this.d = newD;
-      //   this.path.setAttribute('d', newD);
-      //   this.handle.setAttribute('d', newD);
-      // };
-      document.addEventListener('mousemove', this.dragPath.call(chosenLine));
+
+      document.onmousemove = (e) => {
+        let x = e.offsetX;
+        let y = e.offsetY;
+        if (chosenLine) {
+          if (chosenLine.dir === 'left' || chosenLine.dir === 'right') {
+            chosenLine.source.y = y;
+            chosenLine.target.y = y;
+          } else if (chosenLine.dir === 'top' || chosenLine.dir === 'bottom') {
+            chosenLine.source.x = x;
+            chosenLine.target.x = x;
+          }
+        }
+        let newD = '';
+        for (let i = 0; i < this.points.length; i++) {
+          let point = this.points[i];
+          if (i === 0) {
+            newD += `M ${point.x} ${point.y}`;
+          } else {
+            newD += ` L ${point.x} ${point.y}`;
+          }
+        }
+        this.d = newD;
+        this.path.setAttribute('d', newD);
+        this.handle.setAttribute('d', newD);
+        if (chosenLine === this.lastLine) {
+          this.drawArrowEnd();
+        } else if (chosenLine === this.firstLine) {
+          this.drawArrowStart();
+        }
+        this.changeArrowDirection();
+      };
     });
   }
 
@@ -149,81 +164,149 @@ class Edge {
     if (x1 === x2) {
       // return 'vertical';
       if (y1 < y2) {
-        return {
-          type: 'vertical',
-          info: 'top',
-        };
+        return 'bottom';
       } else {
-        return {
-          type: 'vertical',
-          info: 'top',
-        };
+        return 'top';
       }
     } else if (y1 === y2) {
       if (x1 < x2) {
-        return {
-          type: 'horizontal',
-          info: 'right',
-        };
+        return 'right';
       } else {
-        return {
-          type: 'horizontal',
-          info: 'left',
-        };
+        return 'left';
       }
+    }
+  }
+
+  getLineDir(line: Line) {
+    return this.getDir(
+      line.source.x,
+      line.source.y,
+      line.target.x,
+      line.target.y,
+    );
+  }
+
+  addArrowStart() {
+    this.arrowStart = document.createElementNS(xmlns, 'path') as SVGPathElement;
+    this.arrowStart.setAttribute('stroke', 'black');
+    this.arrowStart.setAttribute('fill', 'none');
+    this.arrowStart.setAttribute('stroke-width', '1');
+    this.arrowStart.setAttribute('cursor', 'grab');
+    this.arrowStart.setAttribute('pointer-events', 'bounding-box');
+
+    this.g.appendChild(this.arrowStart);
+
+    this.arrowStartHandle = document.createElementNS(
+      xmlns,
+      'path',
+    ) as SVGPathElement;
+
+    this.arrowStart.addEventListener('mousedown', (e) => {
+      console.log('adf');
+      document.onmousemove = (e) => {
+        this.firstLine.source.y = e.offsetY;
+        this.firstLine.target.y = e.offsetY;
+        this.firstLine.source.x = e.offsetX;
+        let newD = '';
+        for (let i = 0; i < this.points.length; i++) {
+          let point = this.points[i];
+          if (i === 0) {
+            newD += `M ${point.x} ${point.y}`;
+          } else {
+            newD += ` L ${point.x} ${point.y}`;
+          }
+        }
+        this.d = newD;
+        this.path.setAttribute('d', newD);
+        this.handle.setAttribute('d', newD);
+
+        this.drawArrowStart();
+        this.changeArrowDirection();
+
+        document.body.style.cursor = 'grab';
+      };
+    });
+  }
+
+  addArrowEnd() {
+    this.arrowEnd = document.createElementNS(xmlns, 'path') as SVGPathElement;
+    this.arrowEnd.setAttribute('fill', 'black');
+    this.arrowEnd.setAttribute('cursor', 'grab');
+    this.g.appendChild(this.arrowEnd);
+
+    this.arrowEnd.addEventListener('mousedown', (e) => {
+      document.onmousemove = (e) => {
+        this.lastLine.source.y = e.offsetY;
+        this.lastLine.target.y = e.offsetY;
+        this.lastLine.target.x = e.offsetX;
+        let newD = '';
+        for (let i = 0; i < this.points.length; i++) {
+          let point = this.points[i];
+          if (i === 0) {
+            newD += `M ${point.x} ${point.y}`;
+          } else {
+            newD += ` L ${point.x} ${point.y}`;
+          }
+        }
+        this.d = newD;
+        this.path.setAttribute('d', newD);
+        this.handle.setAttribute('d', newD);
+
+        this.drawArrowEnd();
+        this.changeArrowDirection();
+        document.body.style.cursor = 'grab';
+      };
+    });
+  }
+
+  drawArrowStart() {
+    if (this.firstLine.dir === 'right') {
+      let d = '';
+      d += `M ${this.firstLine.source.x - 4} ${this.firstLine.source.y - 4}`;
+      d += ` L ${this.firstLine.source.x} ${this.firstLine.source.y}`;
+      d += ` L ${this.firstLine.source.x - 4} ${this.firstLine.source.y + 4}`;
+      this.arrowStart.setAttribute('d', d);
+    } else if (this.firstLine.dir === 'top') {
+    } else if (this.firstLine.dir === 'bottom') {
+    } else if (this.firstLine.dir === 'left') {
+      let d = '';
+      d += `M ${this.firstLine.source.x + 4} ${this.firstLine.source.y - 4}`;
+      d += ` L ${this.firstLine.source.x} ${this.firstLine.source.y}`;
+      d += ` L ${this.firstLine.source.x + 4} ${this.firstLine.source.y + 4}`;
+      this.arrowStart.setAttribute('d', d);
     }
   }
 
   drawArrowEnd() {
-    let lastLine = this.lines[this.lines.length - 1];
-    if (lastLine.dir.info === 'right') {
-      this.arrowEnd = document.createElementNS(xmlns, 'path') as SVGPathElement;
-      this.arrowEnd.setAttribute('fill', 'black');
+    if (this.lastLine.dir === 'right') {
       let d = '';
-      d += `M ${lastLine.target.x} ${lastLine.target.y - 4}`;
-      d += ` L ${lastLine.target.x + 8} ${lastLine.target.y}`;
-      d += ` L ${lastLine.target.x} ${lastLine.target.y + 4}`;
+      d += `M ${this.lastLine.target.x} ${this.lastLine.target.y - 4}`;
+      d += ` L ${this.lastLine.target.x + 8} ${this.lastLine.target.y}`;
+      d += ` L ${this.lastLine.target.x} ${this.lastLine.target.y + 4}`;
       this.arrowEnd.setAttribute('d', d);
-      this.g.appendChild(this.arrowEnd);
-    } else if (lastLine.dir.info === 'top') {
-      this.arrowEnd = document.createElementNS(xmlns, 'path') as SVGPathElement;
-    } else if (lastLine.dir.info === 'bottom') {
-      this.arrowEnd = document.createElementNS(xmlns, 'path') as SVGPathElement;
-    } else if (lastLine.dir.info === 'left') {
-      this.arrowEnd = document.createElementNS(xmlns, 'path') as SVGPathElement;
+    } else if (this.lastLine.dir === 'top') {
+    } else if (this.lastLine.dir === 'bottom') {
+    } else if (this.lastLine.dir === 'left') {
+      let d = '';
+      d += `M ${this.lastLine.target.x} ${this.lastLine.target.y - 4}`;
+      d += ` L ${this.lastLine.target.x - 8} ${this.lastLine.target.y}`;
+      d += ` L ${this.lastLine.target.x} ${this.lastLine.target.y + 4}`;
+      this.arrowEnd.setAttribute('d', d);
     }
-    this.arrowEnd.addEventListener('mousedown', (e) => {
-      document.onmousemove = () => {};
-    });
   }
 
-  dragPath(chosenLine: Line, e?: MouseEvent) {
-    let x = e.offsetX;
-    let y = e.offsetY;
-    if (chosenLine) {
-      if (chosenLine.dir.type === 'horizontal') {
-        chosenLine.source.y = y;
-        chosenLine.target.y = y;
-      } else if (chosenLine.dir.type === 'vertical') {
-        chosenLine.source.x = x;
-        chosenLine.target.x = x;
-      }
+  changeArrowDirection() {
+    // 如果最后一条线的方向变了，改变 arrowEnd 的 path
+    if (this.firstLine.dir !== this.getLineDir(this.firstLine)) {
+      this.firstLine.dir = this.getLineDir(this.firstLine);
+      this.drawArrowStart();
     }
-    let newD = '';
-    for (let i = 0; i < this.points.length; i++) {
-      let point = this.points[i];
-      if (i === 0) {
-        newD += `M ${point.x} ${point.y}`;
-      } else {
-        newD += ` L ${point.x} ${point.y}`;
-      }
-    }
-    this.d = newD;
-    this.path.setAttribute('d', newD);
-    this.handle.setAttribute('d', newD);
-  }
 
-  dragArrowEnd(e?: MouseEvent) {}
+    if (this.lastLine.dir !== this.getLineDir(this.lastLine)) {
+      this.lastLine.dir = this.getLineDir(this.lastLine);
+      this.drawArrowEnd();
+    }
+  }
 }
 
 let edge = new Edge('M 10 100 L 15 100 L 15 120 L 135 120 L 135 100 L 140 100');
