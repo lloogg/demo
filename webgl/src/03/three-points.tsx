87d47ba7c25c8edca1b1cf1b06cc85749db8db34
wxydev1 <septlog@qq.com>
2023-03-29 09:16:02 +0800

    webgl triangles

---
+++ webgl/src/03/three-points.tsx
@@ -0,0 +1,69 @@
+//@ts-nocheck
+import { useEffect, useRef, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+const VSHADER_SOURCE = `
+attribute vec4 a_Position;
+
+void main() {
+  gl_Position = a_Position;
+  gl_PointSize = 10.0;
+}`;
+
+const FSHADER_SOURCE = `
+precision mediump float;
+uniform vec4 u_Color;
+void main() {
+  gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+}`;
+function App() {
+  const ref = useRef<HTMLCanvasElement>(null);
+  function initVertexBuffers(gl) {
+    const vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+    const n = 3;
+    const vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+      console.log('Fail to create the buffer object');
+      return -1;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+    return n;
+  }
+
+  useEffect(() => {
+    const points = [];
+    let gl = getWebGLContext(ref.current);
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+      console.log('Failed to initialize shaders');
+      return;
+    }
+
+      let n = initVertexBuffers(gl);
+      console.log(n);
+      
+    if (n < 0) {
+      console.log('Failed to set the position of the vertices');
+      return;
+    }
+
+    // let a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    // let u_Color = gl.getUniformLocation(gl.program, 'u_Color');
+    // if (!u_Color) {
+    //   return;
+    // }
+    // gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, n);
+  }, []);
+
+  return <canvas ref={ref}></canvas>;
+}
+
+export default App;
--- webgl/src/App.tsx
+++ webgl/src/App.tsx
@@ -11,8 +11,11 @@ void main() {
   gl_PointSize = 10.0;
 }`;
 
-const FSHADER_SOURCE = `void main() {
-  gl_FragColor = vec4(1.0, 0.5, 0.0, 1.0);
+const FSHADER_SOURCE = `
+precision mediump float;
+uniform vec4 u_Color;
+void main() {
+  gl_FragColor = u_Color;
 }`;
 function App() {
   const ref = useRef<HTMLCanvasElement>(null);
@@ -25,19 +28,29 @@ function App() {
     }
 
     let a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    let u_Color = gl.getUniformLocation(gl.program, 'u_Color');
+    if (!u_Color) {
+      return;
+    }
     gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.drawArrays(gl.POINTS, 0, 1);
 
-
     ref.current.addEventListener('click', (e) => {
-      points.push([e.offsetX, e.offsetY]);
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      points.push([
+        (e.offsetX - ref.current.width / 2) / (ref.current.width / 2),
+        (ref.current.height / 2 - e.offsetY) / (ref.current.height / 2),
+      ]);
       for (let point of points) {
         gl.vertexAttrib3f(a_Position, point[0], point[1], 0.0);
+        gl.uniform4f(u_Color, Math.random(), Math.random(), Math.random(), Math.random());
+        // gl.uniform4f(u_Color, 0.4, 1.0, 1.0, 1.0);
+        gl.drawArrays(gl.POINTS, 0, 1);
       }
-    })
+    });
   }, []);
 
   return (
