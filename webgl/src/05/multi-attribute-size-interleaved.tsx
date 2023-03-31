67bdb3a44a51e6407cf5f62444fa2d1a4874c849
wxydev1 <septlog@qq.com>
2023-03-31 09:28:20 +0800

    多个attribute交错

--- webgl/src/05/multi-attribute-size-interleaved.tsx
+++ webgl/src/05/multi-attribute-size-interleaved.tsx
@@ -2,10 +2,10 @@
 import { useEffect, useRef } from 'react';
 const VSHADER_SOURCE = `
 attribute vec4 a_Position;
-uniform mat4 u_ModelMatrix;
+attribute float a_PointSize;
 void main() {
-  gl_Position = u_ModelMatrix * a_Position ;
-  gl_PointSize = 10.0;
+  gl_Position = a_Position ;
+  gl_PointSize = a_PointSize;
 }`;
 
 const FSHADER_SOURCE = `
@@ -19,7 +19,12 @@ let angleStep = 1;
 function App() {
   const ref = useRef<HTMLCanvasElement>(null);
   function initVertexBuffers(gl) {
-    const vertices = new Float32Array([-0.5, 0.5, -0.5, -0.5, 0.5, 0.5]);
+    //prettier-ignore
+    const verticesSizes = new Float32Array([
+        0, 0.5, 100.0,
+        -0.5, -0.5, 20.0,
+        0.5, -0.5 ,30.0
+    ]);
     const n = 3;
     const vertexBuffer = gl.createBuffer();
     if (!vertexBuffer) {
@@ -28,23 +33,30 @@ function App() {
     }
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, verticesSizes, gl.STATIC_DRAW);
+    let FSIZE = verticesSizes.BYTES_PER_ELEMENT;
     const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
 
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 3, 0);
+
+    gl.vertexAttribPointer(
+      a_PointSize,
+      1,
+      gl.FLOAT,
+      false,
+      FSIZE * 3,
+      FSIZE * 2,
+    );
+
     gl.enableVertexAttribArray(a_Position);
+    gl.enableVertexAttribArray(a_PointSize);
+
     return n;
   }
 
   function main() {
-    function tick() {
-      currentAngle = animate(currentAngle);
-      draw(gl, n, currentAngle, modelMatrix, u_ModelMatrix);
-      requestAnimationFrame(tick);
-    }
-
     let gl = getWebGLContext(ref.current);
-    // 旋转速度（度/秒）
     if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
       console.log('Failed to initialize shaders');
       return;
@@ -57,23 +69,9 @@ function App() {
       return;
     }
 
-    let currentAngle = 0;
-    let modelMatrix = new Matrix4();
-    const u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
-    tick();
-  }
-
-  function draw(gl, n, currentAngle, modelMatrix: Matrix4, u_ModelMatrix) {
-    modelMatrix.setRotate(currentAngle, 0.5, 0.5, 1);
-    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, n);
-  }
-
-  function animate(angle: number) {
-    const newAngle = angle + angleStep;
-    return newAngle % 360;
+    gl.drawArrays(gl.POINTS, 0, n);
   }
 
   useEffect(() => {
--- webgl/src/main.tsx
+++ webgl/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './04/animate'
+import App from './05/multi-attribute-size-interleaved'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
